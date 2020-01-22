a952dba65fe96b7d0c714a3c8363cf76922abc90
rajanski <r.lissewski@parkling.eu>
2020-01-22 11:23:35 +0100

    comment.

--- UBX/ESF.py
+++ UBX/ESF.py
@@ -5,7 +5,7 @@ https://www.u-blox.com/en/docs/UBX-13003221#page=259&zoom=160,-87,770
 #b'\xb5b\x10\x03\x00\x00\x13I'
 
 from UBXMessage import initMessageClass, addGet
-from Types import U,  U4,  X4
+from Types import U,  U4,  X4, U4
 
 
 @initMessageClass
@@ -20,7 +20,7 @@ class ESF:
         _id = 0x03
 
         class Fields:
-            reserved1 = U(1,4)
+            reserved1 = U4(1)#U1(1,4)  works with either U4 or U1(1,4)
 
             class Repeated:
                 data = X4(1)
