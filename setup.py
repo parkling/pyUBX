16997d8c1654a14e7cfd6b65a4776272e04e03ec
rajanski <r.lissewski@parkling.eu>
2020-01-27 13:01:59 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='pyUBX',
+      version='0.0.1',
+      description='A small but functional Python3 wrapper for the u-blox M8 UBX protocol, as defined in UBX-13003221 - R13, §31.',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 3',
+          'Topic :: Communications'
+      ],
+      url='https://github.com/mayeranalytics/pyUBX',
+      author='Mayer Analytics',
+      author_email='info@mayeranalytics.com',
+      license='GNU GPL v3',
+      packages=['pyUBX'],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      )
