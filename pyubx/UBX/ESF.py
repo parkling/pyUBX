7a7cca578fbff4aade5173de136ba44f574c43e5
rajanski <r.lissewski@parkling.eu>
2020-01-27 19:43:42 +0100

    add ESF-MEAS

--- pyubx/UBX/ESF.py
+++ pyubx/UBX/ESF.py
@@ -5,7 +5,7 @@ https://www.u-blox.com/en/docs/UBX-13003221#page=259&zoom=160,-87,770
 #b'\xb5b\x10\x03\x00\x00\x13I'
 
 from pyubx.UBXMessage import initMessageClass, addGet
-from pyubx.Types import X4, U4
+from pyubx.Types import X4, U4, X2, U2
 
 
 @initMessageClass
@@ -25,4 +25,17 @@ class ESF:
             class Repeated:
                 data = X4(1)
                 sTtag = U4(2)
+
+    @addGet
+    class MEAS:
+        _id = 0x02
+
+        class Fields:
+            timeTag = U4(1)  # U1(1,4)  works with either U4 or U1(1,4)
+            flags = X2(2)
+            id = U2(3)
+
+            class Repeated:
+                data = X4(1)
+                calibTtag = U4(2)
 
